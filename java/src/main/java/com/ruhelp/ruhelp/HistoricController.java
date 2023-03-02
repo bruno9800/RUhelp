100fbf7cd3ae58444481fb1ead1580221e59540d
Alan <alanvictor4851@gmail.com>
2023-03-02 13:18:24 -0300

    Add historix box and scrool

--- java/src/main/java/com/ruhelp/ruhelp/HistoricController.java
+++ java/src/main/java/com/ruhelp/ruhelp/HistoricController.java
@@ -1,16 +1,40 @@
 package com.ruhelp.ruhelp;
 
-import java.util.List;
+import java.io.IOException;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TextInputControl;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 
 public class HistoricController {
 
     @FXML private VBox list;
 
-    @FXML private void test(){
-        list.getChildren().add()
+    @FXML private void teste(){
+        for (int i = 0; i < 2; i++) {
+            try {
+                FXMLLoader box = new FXMLLoader(getClass().getResource("historic_box.fxml"));
+                BorderPane boxpane = box.load();
+
+                Node name = boxpane.lookup("#name");
+                ((Label) name).setText("asd");
+
+                Node card = boxpane.lookup("#cardtype");
+                ((Label) card).setText("P1");
+
+                Node date = boxpane.lookup("#date");
+                ((Label) date).setText("%02d/%02d/%02d".formatted(i, i*2, i*3));
+    
+                list.getChildren().add(boxpane);
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
     }
 }

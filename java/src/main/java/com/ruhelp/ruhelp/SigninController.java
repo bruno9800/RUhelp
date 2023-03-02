95bfcb4df81810e30ac0dca5f8bbb79706d2d202
Bruno Matheus Pereira Silva <82674117+bruno9800@users.noreply.github.com>
2023-03-02 14:50:49 -0300

    Tela cadastrar resolvida

--- java/src/main/java/com/ruhelp/ruhelp/SigninController.java
+++ java/src/main/java/com/ruhelp/ruhelp/SigninController.java
@@ -3,16 +3,28 @@ package com.ruhelp.ruhelp;
 import java.io.IOException;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 
 public class SigninController {
     SceneUtils sceneUtils = new SceneUtils();
 
     @FXML
-    //FALTA ADICIONAR INTERAÇÕES COM O BANCO DE DADOS, SÓ TA FAZENDO A TROCA DE TELA
     private void cadastrar(MouseEvent event) {
         try {
-            sceneUtils.SetScene(event, "home.fxml");
+            sceneUtils.SetScene(event, "login.fxml");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    @FXML
+    private void voltar(MouseEvent event) {
+        try {
+            sceneUtils.SetScene(event, "login.fxml");
         } catch (IOException e) {
             e.printStackTrace();
         }
